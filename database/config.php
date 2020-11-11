a131490e91f780d83a4a6357f52316494bf4f191
Mazengia <44846248+mazengia@users.noreply.github.com>
2020-11-11 08:58:37 +0300

    Update config.php

--- database/config.php
+++ database/config.php
@@ -1,10 +1,7 @@
-<?php
-    define('DB_SERVER', '10.180.50.214:3306');
-    define('DB_USERNAME', 'hbceduet');
-    define('DB_PASSWORD', 'qazxsw');
-    define('DB_NAME', 'hbc');
-    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-    if($link === false){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
-    }
+<?php                                      
+$con = mysqli_connect("localhost:3306","hibrebihercom_hbcc","Tm!?+R6k-GO?","hibrebihercom_hbcc");
+if (mysqli_connect_error())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
 ?>

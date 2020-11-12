dd44042d40a9e74e4819efa2b4431a563ca86079
Simonbelete <simonbelete@gmail.com>
2020-11-12 19:21:00 +0300

    Mysql connection config

--- forget/dbcon.php
+++ forget/dbcon.php
@@ -1,7 +1,20 @@
-<?php                                      
-$con = mysqli_connect("10.180.50.214:3306","hbceduet","qazxsw","hbc");
+<?php                
+
+/*$host = "81.19.215.5";
+$port = "3306";
+$database = "hibrebihercom_hibrebihercomdatabase";
+$username = "hibrebihercom_hibrebihercomuser";
+$password = "hibrebihercom_123456789";
+
+$con = mysqli_connect(
+  $host . ":" . $port,
+  $database,
+  $username,
+  $password
+);
+
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+  }*/
 ?>
